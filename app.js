7656b7ea90ff00f7a8acd6eb81e6b505ca64dd76
Aakash Sai Raj Peda Baliyarasimhula <aakash@Aakash-MacBook.local>
2023-06-22 15:08:50 +0530

    change in date.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
 const PORT=process.env.PORT || 3000;
+const date=require(__dirname+'/date.js')
+
 var items=["Buy Food", "Cook Food", "Eat Food"];
 var work=[];
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,12 +14,8 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.get("/",  function(req, res){
-    var today=new Date();
-    var options={
-        weekday:'long', day:"numeric", month:"long"
-    };
-    var day=today.toLocaleDateString("en-US", options);
-    res.render("list", {listTitle:day, newListItems:items});
+    
+    res.render("list", {listTitle:date.getDate(), newListItems:items});
 });
 
 app.post("/", function(req, res){
