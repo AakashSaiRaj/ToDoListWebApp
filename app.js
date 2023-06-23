a762ffaafda4388deaabea482b7612a9d65dd936
Aakash Sai Raj Peda Baliyarasimhula <aakash@Aakash-MacBook.local>
2023-06-23 22:53:09 +0530

    added mongoose to it

--- app.js
+++ app.js
@@ -6,6 +6,71 @@ const bodyParser=require("body-parser");
 const PORT=process.env.PORT || 3000;
 const date=require(__dirname+'/date.js')
 
+
+const mongoose=require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://localhost/todolistDB", ()=>{
+    console.log("DB connected");
+}, e=>console.error(e));
+
+const itemSchema=new mongoose.Schema({
+    item:String
+});
+
+const Item=mongoose.model("Item", itemSchema); 
+
+// run();
+// async function run(){
+//     try{
+//         const item=await Item.create({
+//             item:"buy coconuts"
+//         }); 
+//         console.log(item);
+
+//     }catch(err){
+//         console.log(err.message);
+//     }
+// }
+
+
+
+// run();
+async function run(){
+    try{
+        // const item=await Item.create({
+        //     item:"buy coconuts"
+        // }); 
+        await Item.insertMany([{item:'do home work'},{item:'visit clinic'},{item:'watch show'}]);
+        console.log("Successfully added");
+
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+//inserting items to list
+async function addItem(x){
+    try{
+        await Item.create({item:x});
+        console.log("Successfully added new item");
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+// deleteItem();
+async function deleteItem(x){
+    try{
+        await Item.deleteOne({_id:x});
+        console.log("Deleted successfully");
+
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+
+
 var items=["Buy Food", "Cook Food", "Eat Food"];
 var work=[];
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,8 +79,22 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.get("/",  function(req, res){
+    Item.find({}, function(err, foundItems){
+        if(foundItems.length===0){
+            run(); // check the above code. Its function for adding initial three items.
+            res.render("list", {listTitle:date.getDate(), newListItems:foundItems});
+        }else{
+            res.render("list", {listTitle:date.getDate(), newListItems:foundItems});
+        }
+    });
+
+
+    // Item.find({}, function(err, foundItems){
+        // console.log(foundItems[0]);
+        
+    // });
     
-    res.render("list", {listTitle:date.getDate(), newListItems:items});
+    
 });
 
 app.post("/", function(req, res){
@@ -26,12 +105,17 @@ app.post("/", function(req, res){
         work.push(item);
         res.redirect("/work");
     }else{
-        items.push(item);
+        addItem(item);
+        // inserting new item to list database
         res.redirect("/");
     }
-
     // res.render("list", {newListItem:req.body.newItem});
-    
+});
+
+app.post("/delete", function(req, res){
+    // console.log(req.body);
+    deleteItem(req.body.checkBos);
+    res.redirect("/");
 });
 
 app.get("/work", function(req, res){
